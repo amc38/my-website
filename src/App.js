8329a52e62af319082f55b4bb1eda44e807dbaec
achh888 <amandachan273@gmail.com>
2023-08-08 02:36:21 -0700

    project done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
@@ -6,8 +5,7 @@ import SocialLinks from './components/SocialLinks';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Experience from './components/Experience';
-import Subreddit from './components/Subreddit';
-import { BrowserRouter, Route,Switch } from 'react-router-dom';
+
 
 function App() {
   return (
--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import {FaLinkedin} from 'react-icons/fa'
 import {HiOutlineMail} from 'react-icons/hi'
-import {BsFillPersonLinesFill} from 'react-icons/bs'
-
-
 
 const SocialLinks =() => {
 
